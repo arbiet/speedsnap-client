24eb90b9f75304e87b429503a52aba52226295cb
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2024-07-14 15:39:22 +0700

    asgdsagsga

--- src/Pages/SpeedTest/testUploadSpeed.js
+++ src/Pages/SpeedTest/testUploadSpeed.js
@@ -1,38 +1,60 @@
+// export const testUploadSpeed = async (setUploadSpeed) => {
+//     const largeString = 'a'.repeat(1024 * 10); // 5MB string
+    
+//     const formData = {
+//         upload_data: largeString,
+//     };
+    
+//     const startTime = new Date().getTime();
+//     try {
+//         const response = await fetch('/api/speedtest/upload', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(formData),
+//         });
+    
+//         if (response.status === 401 || response.status === 403) {
+//             console.error('Error:', response.statusText);
+//             return;
+//         }
+//         const endTime = new Date().getTime();
+    
+//         if (response.ok) {
+//             const duration = (endTime - startTime) / 1000;
+//             const bitsLoaded = largeString.length * 8;
+//             const speed = (bitsLoaded / duration / 1024 / 1024).toFixed(2);
+    
+//             setUploadSpeed(speed);
+//             console.log('Upload Test Completed:', `Upload speed: ${speed} Mbps`);
+//         } else {
+//             const errorData = await response.json();
+//             console.error('Upload Test Failed:', errorData.error);
+//         }
+//     } catch (error) {
+//         console.error('Upload Test Failed:', error);
+//     }
+// };
+// src/Pages/SpeedTest/testDownloadSpeed.js
+
 export const testUploadSpeed = async (setUploadSpeed) => {
-    const largeString = 'a'.repeat(1024 * 1024 * 5); // 5MB string
-    
-    const formData = {
-        upload_data: largeString,
-    };
-    
     const startTime = new Date().getTime();
-    try {
-        const response = await fetch('/api/speedtest/upload', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
-    
-        if (response.status === 401 || response.status === 403) {
-            console.error('Error:', response.statusText);
-            return;
-        }
-        const endTime = new Date().getTime();
-    
-        if (response.ok) {
-            const duration = (endTime - startTime) / 1000;
-            const bitsLoaded = largeString.length * 8;
-            const speed = (bitsLoaded / duration / 1024 / 1024).toFixed(2);
-    
-            setUploadSpeed(speed);
-            console.log('Upload Test Completed:', `Upload speed: ${speed} Mbps`);
-        } else {
-            const errorData = await response.json();
-            console.error('Upload Test Failed:', errorData.error);
-        }
-    } catch (error) {
-        console.error('Upload Test Failed:', error);
+    const response = await fetch('/api/speedtest/download', {
+        method: 'GET'
+    });
+    if (response.status === 401 || response.status === 403) {
+        console.error('Error:', response.statusText);
+        return;
     }
+    const data = await response.blob();
+    const endTime = new Date().getTime();
+
+    const duration = (endTime - startTime) / 1000;
+    const bitsLoaded = data.size * 8;
+    const speed = (bitsLoaded / duration / 1024 / 1024).toFixed(2);
+
+    setUploadSpeed(speed);
+    console.log('Download Test Completed:', `Download speed: ${speed} Mbps`);
 };
+
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve('C:/laragon/etc/ssl/laragon.key')), 
+      cert: fs.readFileSync(path.resolve('C:/laragon/etc/ssl/laragon.crt'))
+    },
     proxy: {
       '/api': {
-        // target: 'http://speedsnap.my.id/',
-        target: 'http://127.0.0.1:8000',
+        target: 'https://speedsnap.my.id/',
+        // target: 'http://127.0.0.1:8000',
         changeOrigin:true,
         headers: {
           Accept: 'application/json',
